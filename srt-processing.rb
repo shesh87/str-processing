534544d3d0dce5b06fd3ed403642f107781026bb
Sheonna Harris <svonharris@gmail.com>
2015-01-18 16:56:47 -0500

    A lot harder than I originally thought. Currently trying to get two classes (the file reader & the time converter) to talk to one another.

--- srt-processing.rb
+++ srt-processing.rb
@@ -1,32 +1,66 @@
 #create an srt file
 #when prompted shifts subtile forward or backwards a random amount of miliseconds
+#prints out new srt file with correct times
 
-class SRT
+require "pp"
+require "date"
+require "time"
 
-	def time(direction)
-		@direction = direction.downcase
+
+class TimeShift
+
+	def time(direction, rip_file)
+		@direction = direction
+		now = "2015-01-01 00:00:10,500"
+		time_stamp = Time.parse(now)
+		puts time_stamp
+
 		if direction == "forwards"
-			new_time = current_time + rand(2000..3000)
+			time_new = time_stamp + (rand(2000.0..3300.0)/1000)
+			return time_new.strftime('%H:%M:%S,%3N')
 		elsif direction == "backwards"
-			new_time = current_time - rand(2000..3000)
+			time_new = time_stamp - (rand(2000.0..3300.0)/1000)
+			return time_new.strftime('%H:%M:%S,%3N')
 		else
 			puts "Please enter either Forwards or Backwards."
 		end
 	end
-	def timeshift
-		puts "
-		001
-		00:00:10,500 --> 00:00:13,000
-		Elephant's Dream
-		
-		002
-		00:00:15,000 --> 00:00:18,000
-		At the left we can see...
-		"
-	end
 end
 
 
-srt_file = SRT.new
-# srt_file.time "forward"
-# puts rand(2000..3000)
+class File
+	attr_reader :time1, :time2
+	def initialize(name)
+		@name = name
+	end
+	def readfile
+		subtiles = IO.readlines("subrip.txt")#.split("\n")
+		sub_times = subtiles[1].split(" --> ")
+		@time1 = sub_times[0] #first time
+		@time2 = sub_times[1] #second time
+	end
+end
+
+#_______________________________________________________________
+
+puts "Move the subrip forwards or backwards?"
+direction = gets.chomp
+subrip = File.new "shesh"
+time_stamps = subrip.readfile
+french = TimeShift.new
+puts french.time(direction, time_stamps)
+
+
+
+
+
+
+# cur_time = Time.new(2015,01,01, h,m,sec,milsec)
+cur_time = Time.parse("2015-01-01 00:00:10,500")
+# puts cur_time.strftime('%H:%M:%S,%3N')
+
+
+
+
+
+
